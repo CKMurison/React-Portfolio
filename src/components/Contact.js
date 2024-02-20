0ded6dba0622031f71df919378bc486f3b3972f3
Cameron Murison <cmuris4@gmail.com>
2024-02-20 10:46:06 +0000

    trying original form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const Contact = () => {
-
+export default function Contact() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
@@ -16,56 +15,55 @@ const Contact = () => {
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/.netlify/functions/contact", {
+    fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then(() => {
-        alert("Message sent!");
-        setName("");
-        setEmail("");
-        setMessage("");
-      })
-      .catch((error) => {
-        alert("Message failed to send. Please try again later.");
-        console.error("Error submitting form:", error);
-      });
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
   }
-  
+
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
-          width="100%"
-          height="100%"
-          title="map"
-          className="absolute inset-0"
-          frameBorder={0}
-          marginHeight={0}
-          marginWidth={0}
-          style={{ filter: "opacity(0.7)" }}
-          src="https://www.google.com/maps/embed/v1/place?q=Lincoln,+UK&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-        <div className="bg-gray-900 relative flex flex-wrap py-6 rounder shadow-md">
-          <div className="lg:2-1/2 px-6">
-            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-              EMAIL
-            </h2>
-            <a className="text-indigo-400 leading-relaxed">
-              cmuris4@gmail.com
-             </a>
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                97 Warren St. <br />
+                New York, NY 10007
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                reedbarger@email.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">123-456-7890</p>
+            </div>
           </div>
         </div>
-      </div>
-      <form
+        <form
           netlify
           name="contact"
           onSubmit={handleSubmit}
@@ -74,7 +72,8 @@ const Contact = () => {
             Hire Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Get in touch!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            suscipit officia aspernatur veritatis. Asperiores, aliquid?
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
@@ -111,7 +110,6 @@ const Contact = () => {
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
-
             />
           </div>
           <button
@@ -123,6 +121,4 @@ const Contact = () => {
       </div>
     </section>
   );
-};
- 
-export default Contact;
+}
